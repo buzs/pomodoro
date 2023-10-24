3431a72c83f329f7b55967bf0e47068bc20abfe5
Gabriel Henrique <contato@gabrielhenrique.net>
2023-10-24 20:06:01 +0000

    feat: change prop name and add reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import './App.css'
-import { PomodoroTimer } from './components/Pomodoro/PomodoroTimer'
+import "./App.css";
+import { PomodoroTimer } from "./components/Pomodoro/PomodoroTimer";
 
 function App(): JSX.Element {
   return (
     <>
       <div className="container">
         <h1>Pomodoro</h1>
-        <PomodoroTimer  PomodoroTime={1500} shortRestTime = {300}  longRestTime ={500} cycles={4}/>
+        <PomodoroTimer
+          time={1500}
+          shortRestTime={300}
+          longRestTime={500}
+          cycles={4}
+        />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
